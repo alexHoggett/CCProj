7defdb2e484f7e9db85d75b3ded8fdf7efc24d01
Alex Hoggett <alexhoggett@alexs-mbp-4.eduroam.student>
2020-02-23 16:20:44 +0000

    added audio feature extraction

--- ActivateSectors/src/ofApp.cpp
+++ ActivateSectors/src/ofApp.cpp
@@ -76,10 +76,37 @@ ofVec2f ofApp::cartToPolar(float x, float y){
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    x = y = 0;
-    
     columns = sizeof(blocks) / sizeof(blocks[0]);
     rows = sizeof(blocks[0]) / sizeof(blocks[0][0]);
+    
+    // set up audio things
+    sampleRate             = 44100; /* Sampling Rate */
+    initialBufferSize    = 512;    /* Buffer Size. you have to fill this buffer with sound*/
+    lAudioOut            = new float[initialBufferSize];/* outputs */
+    rAudioOut            = new float[initialBufferSize];
+    lAudioIn            = new float[initialBufferSize];/* inputs */
+    rAudioIn            = new float[initialBufferSize];
+    
+    
+    /* This is a nice safe piece of code */
+    memset(lAudioOut, 0, initialBufferSize * sizeof(float));
+    memset(rAudioOut, 0, initialBufferSize * sizeof(float));
+    
+    memset(lAudioIn, 0, initialBufferSize * sizeof(float));
+    memset(rAudioIn, 0, initialBufferSize * sizeof(float));
+    
+    fftSize = 1024;
+    mfft.setup(fftSize, 512, 256);
+    ifft.setup(fftSize, 512, 256);
+    
+    nAverages = 12;
+    oct.setup(sampleRate, fftSize/2, nAverages);
+    
+    mfccs = (double*) malloc(sizeof(double) * 13);
+    mfcc.setup(512, 42, 13, 20, 20000, sampleRate);
+    
+    ofxMaxiSettings::setup(sampleRate, 2, initialBufferSize);
+    ofSoundStreamSetup(2,2, this, sampleRate, initialBufferSize, 4);/* Call this last ! */
 }
 
 //--------------------------------------------------------------
@@ -92,6 +119,8 @@ void ofApp::update(){
 void ofApp::draw(){
     ofNoFill();
     ofSetColor(0, 255, 0);
+    float horizWidth = 500.;
+    float horizOffset = 100;
     
     int radius = 700;
     
@@ -114,6 +143,7 @@ void ofApp::draw(){
     // draw rects
     for (int i = 0; i < 16; i++){
         for (int j = 0; j < 12; j++){
+            // highlight box where mouse is
             if (mouse.x == i && mouse.y == j){
                 ofFill();
             } else {
@@ -123,10 +153,110 @@ void ofApp::draw(){
         }
     }
     
-    ofDrawEllipse(ofGetMouseX(), ofGetMouseY(), 5, 5);
+    //draw fft output
+    ofFill();
+    float xinc = horizWidth / fftSize * 2.0;
+    for(int i=0; i < fftSize / 2; i++) {
+        //magnitudesDB took out
+        float height = mfft.magnitudes[i] * 100;
+        ofDrawRectangle(horizOffset + (i*xinc),250 - height,2, height);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::audioReceived     (float * input, int bufferSize, int nChannels){
     
-    cout << "Sector: " << whichSectorIndex(ofGetMouseX(), ofGetMouseY(), radius) << endl;
-    cout << whichBlock(ofGetMouseX(), ofGetMouseY()).x << " " << whichBlock(ofGetMouseX(), ofGetMouseY()).y << endl;
+    
+    /* You can just grab this input and stick it in a double, then use it above to create output*/
+    
+    float sum = 0;
+    for (int i = 0; i < bufferSize; i++){
+        
+        /* you can also grab the data out of the arrays*/
+        
+        lAudioIn[i] = input[i*2];
+        rAudioIn[i] = input[i*2+1];
+        
+        sum += input[i*2] * input[i*2];
+        
+    }
+    RMS = sqrt(sum);
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::audioRequested     (float * output, int bufferSize, int nChannels){
+    //    static double tm;
+    
+    
+    
+    for (int i = 0; i < bufferSize; i++){
+        //        wave = osc.saw(maxiMap::linexp(mouseY + ofGetWindowPositionY(), 0, ofGetScreenHeight(), 200, 8000));
+        wave = lAudioIn[i];
+        //wave = samp.play(1.);
+        //get fft
+        if (mfft.process(wave)) {
+            //            int bins   = fftSize / 2.0;
+            //do some manipulation
+            //            int hpCutoff = floor(((mouseX + ofGetWindowPositionX()) / (float) ofGetScreenWidth()) * fftSize / 2.0);
+            //highpass
+            //            memset(mfft.magnitudes, 0, sizeof(float) * hpCutoff);
+            //            memset(mfft.phases, 0, sizeof(float) * hpCutoff);
+            //lowpass
+            //            memset(mfft.magnitudes + hpCutoff, 0, sizeof(float) * (bins - hpCutoff));
+            //            memset(mfft.phases + hpCutoff, 0, sizeof(float) * (bins - hpCutoff));
+            mfft.magsToDB();
+            //            for(int z=0; z < 512; z++) cout << mfft.magnitudesDB[z] << ",";
+            //            cout << "---------\n";
+            oct.calculate(mfft.magnitudesDB);
+            
+            
+            /* for (int j = 0; j < 12; j++) {
+             chromagram[j] = 0;
+             }
+             int j = 0;
+             for (int i = 0; i < oct.nAverages; i++) {
+             chromagram[j] += oct.averages[i];
+             j++;
+             j = j % 12;
+             } */
+            
+            float sum = 0;
+            float maxFreq = 0;
+            int maxBin = 0;
+            
+            for (int i = 0; i < fftSize/2; i++) {
+                sum += mfft.magnitudes[i];
+                if (mfft.magnitudes[i] > maxFreq) {
+                    maxFreq=mfft.magnitudes[i];
+                    maxBin = i;
+                }
+            }
+            centroid = sum / (fftSize / 2);
+            peakFreq = (float)maxBin/fftSize * 44100;
+            
+            
+            mfcc.mfcc(mfft.magnitudes, mfccs);
+            //cout << mfft.spectralFlatness() << ", " << mfft.spectralCentroid() << endl;
+        }
+        //inverse fft
+        gettimeofday(&callTS,NULL);
+        //        ifftVal = ifft.process(mfft.magnitudes, mfft.phases);
+        gettimeofday(&callEndTS,NULL);
+        callTime = (float)(callEndTS.tv_usec - callTS.tv_usec) / 1000000.0;
+        callTime += (float)(callEndTS.tv_sec - callTS.tv_sec);
+        //play result
+        mymix.stereo(wave, outputs, 0.5);
+        //        float mix = ((mouseX + ofGetWindowPositionX()) / (float) ofGetScreenWidth());
+        //        mymix.stereo((wave * mix) + ((1.0-mix) * ifftVal), outputs, 0.5);
+        // lAudioOut[i] = output[i*nChannels    ] = outputs[0]; /* You may end up with lots of outputs. add them here */
+        //rAudioOut[i] = output[i*nChannels + 1] = outputs[1];
+        lAudioOut[i] = 0;
+        rAudioOut[i] = 0;
+        
+    }
+    
+    
     
 }
 
--- ActivateSectors/src/ofApp.h
+++ ActivateSectors/src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxMaxim.h"
 #include <math.h>
 
 class ofApp : public ofBaseApp{
@@ -26,15 +27,51 @@ class ofApp : public ofBaseApp{
         ofVec2f cartToPolar(float x, float y);
         ofVec2f whichBlock(float x, float y);
     
-        int height, width;
-        int x, y;
+        void audioReceived     (float * input, int bufferSize, int nChannels); /* input method */
+        void audioRequested     (float * input, int bufferSize, int nChannels); /* output method */
     
+        int height, width;
         int whichSectorIndex(float x, float y, int radius);
     
         float angles [12] = {PI/6, PI/3, PI/2, PI*2/3, PI*5/6, PI, PI*7/6, PI*4/3, PI*3/2, PI*5/3, PI*11/6, 2*PI};
     
         int rows;
         int columns;
-    
         int blocks [16][12];
+    
+        //  Maxim stuff
+        float   * lAudioOut; /* outputs */
+        float   * rAudioOut;
+    
+        float * lAudioIn; /* inputs */
+        float * rAudioIn;
+    
+        int        initialBufferSize; /* buffer size */
+        int        sampleRate;
+    
+        double wave,sample,outputs[2], ifftVal;
+        maxiMix mymix;
+        maxiOsc osc;
+    
+        ofxMaxiFFTOctaveAnalyzer oct;
+        int nAverages;
+        float *ifftOutput;
+        int ifftSize;
+    
+        float peakFreq = 0;
+        float centroid = 0;
+        float RMS = 0;
+    
+        ofxMaxiIFFT ifft;
+        ofxMaxiFFT mfft;
+        int fftSize;
+        int bins, dataSize;
+    
+        float callTime;
+        timeval callTS, callEndTS;
+    
+        maxiMFCC mfcc;
+        double *mfccs;
+    
+        maxiSample samp;
 };
